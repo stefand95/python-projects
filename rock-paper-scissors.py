2fe2d65bc6ed617160f430e745bca783fa65b316
stefand95 <stefann.dulcaa@gmail.com>
2022-07-25 11:09:05 +0300

    Feat: Rock, Paper, Scissors Game with score and loop

--- rock-paper-scissors.py
+++ rock-paper-scissors.py
@@ -1,33 +1,43 @@
 from random import randint
 
-print("...rock...\n...paper...\n...scissors...")
+player_wins = 0
+computer_wins = 0
+winning_score = 3
 
-player = input("What do you choose? ").lower()
-computer = randint(0,2)
+while player_wins < winning_score and computer_wins < winning_score:
 
-if computer == 0:
-    computer = "rock"
-elif computer == 1:
-    computer = "paper"
-else:
-    computer = "scissors"
-print(f"Computer chose {computer}")
+    print(f"Player Score: {player_wins} Computer Score: {computer_wins}")
+    print("...Rock...\n...Paper...\n...Scissors...")
+    player = input("What do you choose? ").lower()
+    if player == "quit" or player == "q":
+        break
+    computer = randint(0,2)
 
-if player == computer:
-    print("It's a tie!")
-
-elif player == "rock":
-    if computer == "scissors": 
-        print("You win!")
+    if computer == 0:
+        computer = "rock"
+    elif computer == 1:
+        computer = "paper"
     else:
-        print("You lose!")
-elif player == "paper":
-    if computer == "rock": 
-        print("You win!")
+        computer = "scissors"
+    print(f"Computer chose {computer}")
+
+    if player == computer:
+        print("It's a tie!")
+
+    elif player == "rock":
+        if computer == "scissors": 
+            print("You win!")
+        else:
+            print("You lose!")
+    elif player == "paper":
+        if computer == "rock": 
+            print("You win!")
+        else:
+            print("You lose!")
+    elif player == "scissors":
+        if computer == "paper": 
+            print("You win!")
+        else:
+            print("You lose!")
     else:
-        print("You lose!")
-elif player == "scissors":
-    if computer == "paper": 
-        print("You win!")
-    else:
-        print("You lose!")
+        print("Please enter a valid move!")
