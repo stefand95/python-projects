017d96ab2a0e237a7fb47923e929560f4f24d2bc
stefand95 <stefann.dulcaa@gmail.com>
2022-07-25 11:00:36 +0300

    Feat: Rock-Paper-Scissors Game

--- rock-paper-scissors.py
+++ rock-paper-scissors.py
@@ -2,7 +2,7 @@ from random import randint
 
 print("...rock...\n...paper...\n...scissors...")
 
-player = input("What do you choose? ")
+player = input("What do you choose? ").lower()
 computer = randint(0,2)
 
 if computer == 0:
@@ -15,15 +15,19 @@ print(f"Computer chose {computer}")
 
 if player == computer:
     print("It's a tie!")
-elif player == "rock" and computer == "scissors": 
-    print("Player wins!")
-elif player == "scissors" and computer == "rock": 
-    print("Computer wins!")
-elif player == "paper" and computer == "rock": 
-    print("Player wins!")
-elif player == "rock" and computer == "paper": 
-    print("Computer wins!")
-elif player == "scissors" and computer == "paper": 
-    print("Player wins!")
-elif player == "paper" and computer == "scissors":  
-    print("Computer wins!")
+
+elif player == "rock":
+    if computer == "scissors": 
+        print("You win!")
+    else:
+        print("You lose!")
+elif player == "paper":
+    if computer == "rock": 
+        print("You win!")
+    else:
+        print("You lose!")
+elif player == "scissors":
+    if computer == "paper": 
+        print("You win!")
+    else:
+        print("You lose!")
